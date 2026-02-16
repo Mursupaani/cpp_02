cfbff0a7d7582d698c8251943f06bf0d14d407c2
Antti Pollanen <antti.pollanen89@gmail.com>
2026-02-16 17:40:34 +0200

    Added make fclean to delete compile_commands.json

---
+++ ex03/src/Fixed.cpp
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 10:26:16 by anpollan          #+#    #+#             */
+/*   Updated: 2026/02/10 10:26:18 by anpollan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : _rawBits(0)
+{}
+
+Fixed::Fixed(const Fixed &other)
+{
+	_rawBits = other._rawBits;
+}
+
+Fixed::Fixed(int const value)
+{
+	_rawBits = (value << _fractionalBits);
+}
+
+Fixed::Fixed(float const value)
+{
+	_rawBits = std::roundf(value * (1 << _fractionalBits));
+}
+
+Fixed::~Fixed(void)
+{}
+
+int Fixed::getRawBits(void) const
+{
+	return (_rawBits);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_rawBits = raw;
+}
+
+int Fixed::toInt(void) const
+{
+	return (_rawBits >> _fractionalBits);
+}
+
+float Fixed::toFloat(void) const
+{
+	return (static_cast<float>(_rawBits) / (1 << _fractionalBits));
+}
+
+std::ostream &operator<<(std::ostream &stream, const Fixed &fixed)
+{
+	stream << fixed.toFloat();
+	return (stream);
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	if (this == &other)
+		return (*this);
+	_rawBits = other._rawBits;
+	return (*this);
+}
+
+bool Fixed::operator>(const Fixed &other) const
+{
+	return (this->_rawBits > other._rawBits);
+}
+
+bool Fixed::operator<(const Fixed &other) const
+{
+	return (this->_rawBits < other._rawBits);
+}
+
+bool Fixed::operator>=(const Fixed &other) const
+{
+	return (this->_rawBits >= other._rawBits);
+}
+
+bool Fixed::operator<=(const Fixed &other) const
+{
+	return (this->_rawBits <= other._rawBits);
+}
+
+bool Fixed::operator==(const Fixed &other) const
+{
+	return (this->_rawBits == other._rawBits);
+}
+
+bool Fixed::operator!=(const Fixed &other) const
+{
+	return (this->_rawBits != other._rawBits);
+}
+
+Fixed Fixed::operator+(const Fixed &other)
+{
+	Fixed sum;
+	int rawBits = _rawBits + other._rawBits;
+	sum.setRawBits(rawBits);
+	return (sum);
+}
+
+Fixed Fixed::operator-(const Fixed &other)
+{
+	Fixed result;
+	int rawBits = _rawBits - other._rawBits;
+	result.setRawBits(rawBits);
+	return (result);
+}
+
+Fixed Fixed::operator*(const Fixed &other)
+{
+	Fixed result;
+	int rawBits = _rawBits * other._rawBits;
+	result.setRawBits(rawBits >> _fractionalBits);
+	return (result);
+}
+
+Fixed Fixed::operator/(const Fixed &other)
+{
+	Fixed division;
+	int rawBits = (_rawBits << _fractionalBits) / other._rawBits;
+	division.setRawBits(rawBits);
+	return (division);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed	temp = *this;
+	_rawBits++;
+	return (temp);
+}
+
+Fixed &Fixed::operator++()
+{
+	_rawBits++;
+	return (*this);
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
---
+++ ex03/src/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 10:26:20 by anpollan          #+#    #+#             */
+/*   Updated: 2026/02/10 10:26:22 by anpollan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	{
+		Fixed a(3);
+		Fixed b(4);
+		Fixed c = a;
+
+		std::cout << a << " > " << b << " : " << (a > b) << std::endl;
+		std::cout << a << " < " << b << " : " << (a < b) << std::endl;
+		std::cout << a << " >= " << b << " : " << (a >= b) << std::endl;
+		std::cout << a << " <= " << b << " : " << (a <= b) << std::endl;
+		std::cout << a << " == " << b << " : " << (a == b) << std::endl;
+		std::cout << a << " != " << b << " : " << (a != b) << std::endl;
+		std::cout << a << " > " << c << " : " << (a > c) << std::endl;
+		std::cout << a << " < " << c << " : " << (a < c) << std::endl;
+		std::cout << a << " >= " << c << " : " << (a >= c) << std::endl;
+		std::cout << a << " <= " << c << " : " << (a <= c) << std::endl;
+		std::cout << a << " == " << c << " : " << (a == c) << std::endl;
+		std::cout << a << " != " << c << " : " << (a != c) << std::endl;
+		std::cout << a << " + " << b << " = " << (a + b) << std::endl;
+		std::cout << a << " - " << b << " = "  << (a - b) << std::endl;
+		std::cout << a << " * " << b << " = "  << (a * b) << std::endl;
+		std::cout << a << " / " << b << " = "  << (a / b) << std::endl;
+	}
+	{
+		Fixed a;
+		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+		Fixed c = 10;
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max( a, b ) << std::endl;
+		std::cout << Fixed::min( a, b ) << std::endl;
+		std::cout << Fixed::min(a, c) << std::endl;
+		std::cout << ++Fixed::min(a, c) << std::endl;
+		std::cout << Fixed::min(a, c) << std::endl;
+	}
+	return (EXIT_SUCCESS);
+}
